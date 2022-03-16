bfcc18e86df89a52e7fb086c7d58895a2db724b1
giansingh4710 <giansingh4710@gmail.com>
2022-03-15 23:38:35 -0400

    made header component. was trying to style it. then found out react-native-elements has a Header so trying that

--- src/openPdf.js
+++ src/openPdf.js
@@ -7,6 +7,7 @@ import {
     TextInput,
     Alert,
 } from 'react-native';
+import {Icon, Header} from 'react-native-elements';
 import {RightOfHeader} from '../assets/components/rightOfHeader';
 
 import Pdf from 'react-native-pdf';
@@ -14,6 +15,7 @@ import TeekaPDF from '../assets/components/teekaPdf';
 import {useSelector, useDispatch} from 'react-redux';
 import {setAngNum} from '../redux/actions';
 import {allColors} from '../assets/styleForEachOption';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export default function OpenPdf({navigation, route}) {
     const [totalAngs, setTotalAngs] = React.useState(0);
@@ -21,7 +23,6 @@ export default function OpenPdf({navigation, route}) {
     const [showHeader, setShowHeader] = React.useState(true);
 
     const currentAngRef = React.useRef(1); //only for addListner
-    //console.log("Test rendering",currentAng,currentAngRef)
 
     const state = useSelector(theState => theState.theReducer);
     const dispatch = useDispatch();
@@ -30,10 +31,17 @@ export default function OpenPdf({navigation, route}) {
 
     const fileName = pdfTitle.split(' ').join(''); //replaces " " with ""
     const sourceFileName = {uri: state.allPdfs[pdfTitle].uri};
+    //const sourceFileName = { uri: 'http://kathadata.host/pdfs/BaiVarra/1)SriRaagKiVaarMahala4.pdf', cache: true };
 
     if (fileName === 'FareedkotTeeka.pdf') {
         return <TeekaPDF navigation={navigation} />;
     }
+
+    React.useEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
 
     React.useEffect(() => {
         //pick up where you left off from last time
@@ -44,144 +52,6 @@ export default function OpenPdf({navigation, route}) {
         });
     }, [totalAngs, navigation]);
 
-    const headerStyles = StyleSheet.create({
-        container: {
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-        },
-        title: {
-            flex: 1,
-            margin: 10,
-            borderRadius: 5,
-            height: '75%',
-            backgroundColor: '#077b8a',
-            justifyContent: 'center',
-            textAlign: 'center',
-        },
-        pagesBox: {
-            flex: 1.5,
-            margin: 10,
-            borderRadius: 5,
-            alignItems: 'center',
-            justifyContent: 'center',
-            // justifyContent: 'space-evenly',
-            fontSize: 20,
-            flexDirection: 'row',
-            backgroundColor: '#077b8a',
-        },
-        setAngNumBox: {
-            borderRadius: 5,
-            height: '95%',
-            fontSize: 20,
-            backgroundColor: '#a2d5c6',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-        },
-        boxText: {
-            fontSize: 25,
-        },
-        headerBtnsCont: {
-            flex: 1,
-            flexDirection: 'row',
-        },
-        headerBtns: {
-            flex: 1,
-            paddingTop: 20,
-        },
-    });
-
-    {
-        /*
-    React.useEffect(() => {
-        let showTitle = pdfTitle;
-        if (showTitle.length > 15) showTitle = showTitle.slice(0, 15) + '...';
-        console.log('YOYOYOO');
-        navigation.setOptions({
-            headerShown: showHeader,
-            headerTintColor: state.darkMode ? 'white' : 'black',
-            headerStyle: {
-                backgroundColor: allColors[state.darkMode].headerColor,
-            },
-            headerTitle: () => {
-                return (
-                    <View style={headerStyles.container}>
-                        <Text style={headerStyles.title}>{showTitle}</Text>
-                        <View style={headerStyles.pagesBox}>
-                            <TextInput
-                                keyboardType="numeric"
-                                value={currentAng.toString()}
-                                style={headerStyles.setAngNumBox}
-                                placeholder="ex: 5"
-                                onChangeText={text => {
-                                    if (text.length === 0) {
-                                        setCurrentAng('');
-                                    } else if (text.length < 5) {
-                                        setCurrentAng(parseInt(text));
-                                    }
-                                }}
-                                onSubmitEditing={e => {
-                                    const asInt = currentAng;
-                                    if (asInt) {
-                                        //this.pdf.setPage(asInt);
-                                        if (asInt > totalAngs) {
-                                            setCurrentAng(totalAngs);
-                                        }
-                                    }
-                                }}
-                            />
-                            <Text style={headerStyles.boxText}>
-                                /{totalAngs}
-                            </Text>
-                        </View>
-                    </View>
-                );
-            },
-            headerRight: () => (
-                <RightOfHeader
-                    state={state}
-                    icons={[
-                        {
-                            name: 'shuffle-outline',
-                            action: () => {
-                                const randAng =
-                                    Math.floor(Math.random() * totalAngs) + 1;
-                                //this.pdf.setPage(randAng);
-                                setCurrentAng(randAng);
-                            },
-                        },
-                        {
-                            name: 'settings-outline',
-                            action: () => {
-                                navigation.navigate('Settings Page');
-                            },
-                        },
-                    ]}
-                />
-            ),
-        });
-    });
-
-*/
-    }
-
-    React.useEffect(() => {
-        navigation.setOptions({
-            headerTintColor: state.darkMode ? 'white' : 'black',
-            headerStyle: {
-                backgroundColor: allColors[state.darkMode].headerColor,
-            },
-            headerTitle: () => (
-                <View style={styles.pagesBox}>
-                    <Text style={styles.boxText}>
-                        {currentAng}/{totalAngs}
-                    </Text>
-                </View>
-            ),
-            headerShown: showHeader,
-        });
-    });
-    console.log("rerendering: ",currentAng,currentAngRef)
     const styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -193,24 +63,35 @@ export default function OpenPdf({navigation, route}) {
             width: '100%',
             height: '100%',
             borderRadius: 15,
+            //position: 'absolute'
         },
     });
     return (
         <View style={styles.container}>
+            <Header1
+                title={pdfTitle}
+                currentAng={currentAng}
+                totalAngs={totalAngs}
+                state={state}
+                navigation={navigation}
+            />
+            {/*
+
             <Pdf
                 ref={pdf => {
                     //this.pdf = pdf;
                 }}
+                source={sourceFileName}
                 activityIndicator={
                     <ActivityIndicator size="large" color="blue" />
                 }
-                source={sourceFileName}
                 onLoadComplete={(numberOfPages, filePath) => {
                     setTotalAngs(numberOfPages);
                 }}
                 onPageChanged={(page, numberOfPages) => {
                     setCurrentAng(page);
                     //navigation.setOptions({title:page})
+                    //navigation.goBack();
                     if (
                         showHeader &&
                         state.hideHeaderOnScroll &&
@@ -244,6 +125,188 @@ export default function OpenPdf({navigation, route}) {
                 }}
                 style={styles.pdf}
             />
+*/}
+        </View>
+    );
+}
+
+function Header1({title, currentAng, totalAngs, state, navigation}) {
+    const styles = StyleSheet.create({
+        container: {
+            //flex:1,
+            alignItems: 'center',
+            backgroundColor: allColors[state.darkMode].headerColor,
+            width: '100%',
+            height: '10%',
+            //top: 30,
+            //position:'absolute',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            //padding:15,
+        },
+        angInfo: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            //fontSize: 20,
+            borderRadius: 10,
+            //padding:5,
+            backgroundColor: '#077b8a',
+        },
+        title: {
+            backgroundColor: 'red',
+            textAlign: 'center',
+        },
+        angNumInfo: {
+            //padding:10,
+            //margin:5,
+            backgroundColor: '#078b8a',
+            flexDirection: 'row',
+            alignItems: 'center',
+        },
+        setAngNumBox: {
+            //top:"2%",
+            borderRadius: 5,
+            backgroundColor: '#a2d5c6',
+            textAlign: 'right',
+            fontSize: 25,
+        },
+        totalAngsInfo: {
+            fontSize: 25,
+        },
+        headerBtns: {},
+
+        headerContainer: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#397af8',
+            marginBottom: 20,
+            width: '100%',
+            paddingVertical: 15,
+        },
+        heading: {
+            color: 'white',
+            fontSize: 22,
+            fontWeight: 'bold',
+        },
+        headerRight: {
+            display: 'flex',
+            flexDirection: 'row',
+            marginTop: 5,
+        },
+        subheaderText: {
+            color: 'white',
+            fontSize: 16,
+            fontWeight: 'bold',
+        },
+    });
+    let showTitle = title;
+    //if (showTitle.length > 10) showTitle = showTitle.slice(0, 10) + '...';
+    const iconsSize = 25;
+    return (
+        <Header
+            leftComponent={
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}
+                    style={styles.headerBtns}>
+                    <Icon
+                        name="arrow-back-outline"
+                        size={iconsSize}
+                        type="ionicon"
+                        color={state.darkMode ? 'white' : 'black'}
+                    />
+                </TouchableOpacity>
+            }
+            rightComponent={
+                <View style={styles.headerRight}>
+                    <TouchableOpacity
+                        style={styles.headerBtns}
+                        onPress={() => {}}>
+                        <Icon
+                            size={iconsSize}
+                            name="shuffle-outline"
+                            type="ionicon"
+                            color={state.darkMode ? 'white' : 'black'}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.headerBtns}
+                        onPress={() => {
+                            navigation.navigate('Settings Page');
+                        }}>
+                        <Icon
+                            size={iconsSize}
+                            name="settings-outline"
+                            type="ionicon"
+                            color={state.darkMode ? 'white' : 'black'}
+                        />
+                    </TouchableOpacity>
+                </View>
+            }
+            centerComponent={{text: 'Header', style: styles.heading}}
+        />
+    );
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                style={styles.headerBtns}>
+                <Icon
+                    name="arrow-back-outline"
+                    size={iconsSize}
+                    type="ionicon"
+                    color={state.darkMode ? 'white' : 'black'}
+                />
+            </TouchableOpacity>
+            <View style={styles.angInfo}>
+                <Text style={styles.title}>{showTitle}</Text>
+                <View style={styles.angNumInfo}>
+                    <TextInput
+                        style={styles.setAngNumBox}
+                        keyboardType="numeric"
+                        //value={'1456'}
+                        //value={currentAng.toString()}
+                        placeholder={currentAng.toString()}
+                        //onSubmitEditing={e => {
+                        //const asInt = currentAng;
+                        //if (asInt) {
+                        //// this.pdf.setPage(asInt);
+                        //if (asInt > totalAngs) {
+                        ////setCurrentAng(totalAngs);
+                        //}
+                        //}
+                        //}}
+                        //onChangeText={text => {
+                        //console.log(text)
+                        //if (text.length === 0) {
+                        ////setCurrentAng('');
+                        //} else if (text.length < 5) {
+                        ////setCurrentAng(parseInt(text));
+                        //}
+                        //}}
+                    />
+                    <Text style={styles.totalAngsInfo}>/{totalAngs}</Text>
+                </View>
+            </View>
+            <TouchableOpacity style={styles.headerBtns} onPress={() => {}}>
+                <Icon
+                    size={iconsSize}
+                    name="shuffle-outline"
+                    type="ionicon"
+                    color={state.darkMode ? 'white' : 'black'}
+                />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.headerBtns}
+                onPress={() => {
+                    navigation.navigate('Settings Page');
+                }}>
+                <Icon
+                    size={iconsSize}
+                    name="settings-outline"
+                    type="ionicon"
+                    color={state.darkMode ? 'white' : 'black'}
+                />
+            </TouchableOpacity>
         </View>
     );
 }
